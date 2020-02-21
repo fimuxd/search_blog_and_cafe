5f08cee1f56851d68dd3f16a26c158196262259c
fimuxd <fimuxd@gmail.com>
2020-02-21 20:13:52 +0900

    Initial Commit

---
+++ search_blog_and_cafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  search_blog_and_cafe
+//
+//  Created by Bo-Young PARK on 2020/02/21.
+//  Copyright © 2020 Boyoung Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
