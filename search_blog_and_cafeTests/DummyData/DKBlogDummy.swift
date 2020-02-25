3108d5fff4ac27a889a77e1173355b8510a220ef
박보영 <boyoung@rainist.com>
2020-02-25 17:38:28 +0900

    add dummy data

---
+++ search_blog_and_cafeTests/DummyData/DKBlogDummy.swift
@@ -0,0 +1,14 @@
+//
+//  DKBlogDummy.swift
+//  search_blog_and_cafeTests
+//
+//  Created by Bo-Young PARK on 2020/02/25.
+//  Copyright © 2020 Boyoung Park. All rights reserved.
+//
+
+import Foundation
+@testable import search_blog_and_cafe
+
+struct DKBlogDummy {
+    let blog0 = DKBlog(meta: DKMetaDummy().meta0, documents: [DKDocumentDummy().blogDoc0, DKDocumentDummy().blogDoc1, DKDocumentDummy().blogDoc2, DKDocumentDummy().blogDoc3, DKDocumentDummy().blogDoc4, DKDocumentDummy().blogDoc5, DKDocumentDummy().blogDoc6, DKDocumentDummy().blogDoc7, DKDocumentDummy().blogDoc8, DKDocumentDummy().blogDoc9, DKDocumentDummy().blogDoc10, DKDocumentDummy().blogDoc11, DKDocumentDummy().blogDoc12, DKDocumentDummy().blogDoc13, DKDocumentDummy().blogDoc14, DKDocumentDummy().blogDoc15, DKDocumentDummy().blogDoc16, DKDocumentDummy().blogDoc17, DKDocumentDummy().blogDoc18, DKDocumentDummy().blogDoc19, DKDocumentDummy().blogDoc20, DKDocumentDummy().blogDoc21, DKDocumentDummy().blogDoc22, DKDocumentDummy().blogDoc23, DKDocumentDummy().blogDoc24, DKDocumentDummy().blogDoc25, DKDocumentDummy().blogDoc26, DKDocumentDummy().blogDoc27, DKDocumentDummy().blogDoc28, DKDocumentDummy().blogDoc29])
+}
